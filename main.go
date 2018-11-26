973cef732cd4c5b9695cbf97f178d1f78ca9cc48
徐成鹏 <xuchengpeng.lu@bytedance.com>
2018-11-26 17:04:34 +0800

    add go
    Change-Id: Id628e6cecedec9fb84d9ff4b33479dd69c0ac258

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+)
+
+func main() {
+	// 读取 conf 配置，初始化数据库
+	maxIdle, _ := beego.AppConfig.Int("maxIdle")
+	maxConn, _ := beego.AppConfig.Int("maxConn")
+	datasource := beego.AppConfig.String("datasource")
+	orm.RegisterDataBase("default", "mysql", datasource, maxIdle, maxConn)
+
+
+	if beego.BConfig.RunMode == "dev" {
+		orm.Debug = true
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+}
